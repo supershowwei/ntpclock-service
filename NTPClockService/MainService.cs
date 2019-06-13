9d44cb5faaed7fb292993ec9d58f44409aa197b7
johnny <johnny@wantgoo.com>
2019-06-13 12:22:12 +0800

    first commit

---
+++ NTPClockService/MainService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace NTPClockService
+{
+    internal class MainService
+    {
+        private static readonly string CurrentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        private bool isServieStopped;
+        private Process process;
+
+        public void Start()
+        {
+            this.process = new Process
+                           {
+                               StartInfo = { FileName = Path.Combine(CurrentDirectory, "NTPClock.exe"), UseShellExecute = false },
+                               EnableRaisingEvents = true
+                           };
+
+            this.process.Exited += this.ProcessOnExited;
+
+            this.process.Start();
+        }
+
+        public void Stop()
+        {
+            this.isServieStopped = true;
+
+            this.process?.Kill();
+        }
+
+        private void ProcessOnExited(object sender, EventArgs e)
+        {
+            if (!this.isServieStopped)
+            {
+                Environment.Exit(int.MinValue);
+            }
+        }
+    }
+}
---
+++ NTPClockService/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Topshelf;
+
+namespace NTPClockService
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var rc = HostFactory.Run(
+                hc =>
+                    {
+                        hc.SetServiceName("NTPClockService");
+                        hc.SetDisplayName("NTPClock Service");
+
+                        hc.StartAutomatically();
+
+                        hc.RunAsLocalSystem();
+
+                        hc.EnableServiceRecovery(
+                            sr =>
+                                {
+                                    sr.RestartService(0);
+                                    sr.RestartService(1);
+                                    sr.RestartService(1);
+
+                                    sr.SetResetPeriod(1);
+
+                                    sr.OnCrashOnly();
+                                });
+
+                        hc.Service<MainService>(
+                            sc =>
+                                {
+                                    sc.ConstructUsing(hs => new MainService());
+
+                                    sc.WhenStarted(s => s.Start());
+                                    sc.WhenStopped(s => s.Stop());
+                                });
+                    });
+
+            Environment.ExitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+        }
+    }
+}
